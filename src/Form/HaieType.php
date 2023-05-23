a62264a38de18b4e0a374576f6b51201c4f2336a
adminlocal <adminlocal@PC-00283-SVL.0870019Y.lan>
2023-05-23 15:19:14 +0200

    probléme régler sur autre pc

--- src/Form/HaieType.php
+++ src/Form/HaieType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Haie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class HaieType extends AbstractType
@@ -12,10 +14,10 @@ class HaieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('code')
-            ->add('nom')
-            ->add('prix')
-            ->add('categorie')
+            ->add('code', TextType::class, array('attr' => ['class' => 'form-mid']))
+            ->add('nom', TextType::class, array('attr' => ['class' => 'form-mid']))
+            ->add('prix', TextType::class, array('attr' => ['class' => 'form-mid']))
+            ->add('categorie', EntityType::class, ['class' => 'App\Entity\Categorie', 'choice_label' => 'libelle'])
         ;
     }
 
